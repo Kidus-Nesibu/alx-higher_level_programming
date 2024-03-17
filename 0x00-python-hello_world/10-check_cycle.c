e7d69a50a982da1231116277eb7fd5469d16d0f5
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-17 01:57:09 -0700

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include"lists.h"
+/**
+ * check_cycle - check if there is a cycle in linked list
+ * @list: is the head of the linked list
+ * Return: 0 0r one depending on the result
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr1, *ptr2;
+
+	ptr1 = list;
+	ptr2 = list;
+
+	if (list == NULL || list->next == NULL)
+	{
+		return (0);
+	}
+
+	while (ptr2 != NULL)
+	{
+		ptr1 = ptr1->next;
+		ptr2 = ptr2->next->next;
+
+		if (ptr1 == ptr2)
+		{
+			return (1);
+		}
+
+	}
+	return (0);
+}

995347274362229c6b52abf91b2c5658b209b6c0
Kidus-Nesibu <knesibu3@gmail.com>
2024-03-19 04:04:16 -0700

    readme file

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def remove_char_at(str, n):
-    update = ""
-    i = 0
-    for c in str:
-        if i != n:
-            update += c
-        i += 1
-    return update
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in range(len(str)):
-        letter = ord(str[i])
-        if letter >= 97 and letter <= 122:
-            letter = letter - 32
-        print("{}".format(chr(letter)), end='')
-    print()

b808fd9b7577d12ee59d5e91c0d6ec457720e54b
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-05 13:27:24 +0300

    task 13

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,7 +1,6 @@
 import dis
 def magic_calculation(a, b):
     result = 98
-    result += a
-    result += b
-    result = result ** 2
+    result = a + b ** result
     return result
+dis.dis(magic_calculation)

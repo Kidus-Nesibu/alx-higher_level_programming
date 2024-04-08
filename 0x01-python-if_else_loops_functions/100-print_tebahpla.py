81dc20397cf2a59efd1368193d040c0eb848b194
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-08 12:40:33 +0300

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 a = 'z'
-
+i = 1
 while ord('a') <= ord(a):
-    print(a)
+    if i % 2 == 0:
+        print(a.upper(), end="")
+    else:
+        print(a, end="")
     a = chr(ord(a)-1)
+    i += 1

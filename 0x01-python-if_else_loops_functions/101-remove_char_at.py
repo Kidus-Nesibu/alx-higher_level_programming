c9b07628ad73c26953090b9cabd0e619af657d0d
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-08 13:50:57 +0300

    task 16

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,2 +1,10 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python3i
+def remove_char_at(str, n):
+    tmp = ""
+    length = len(str)
+    i = 0
+    while i < length:
+        if str[i] != str[n]:
+            tmp += str[i]
+        i += 1
+    return tmp

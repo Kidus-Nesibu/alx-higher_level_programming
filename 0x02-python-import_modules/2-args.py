eccea5ec50984c8f2627584db4d67a464aba81f4
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-10 17:53:07 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 num_arg = len(sys.argv)
 args = sys.argv
 i = 1
@@ -8,5 +9,3 @@ print("{} argument:".format(num_arg - 1))
 while i < num_arg:
     print("{}: {}".format(i, args[i]))
     i += 1
-if __name__ == "__main__":
-    import sys

f4f77da84f30f96f8b5c339300d8ee95b227bbd8
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-10 18:08:54 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ num_arg = len(sys.argv)
 args = sys.argv
 i = 1
 if num_arg - 1 == 0:
-    print("{}argument.".format(num_arg - 1))
+    print("{} argument.".format(num_arg - 1))
 else:
-    print("{}argument:".format(num_arg - 1))
+    print("{} argument:".format(num_arg - 1))
 while i < num_arg:
     print("{}:{}".format(i, args[i]))
     i += 1

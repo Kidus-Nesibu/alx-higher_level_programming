07646cb06f918011fe9d419a553b77a291f24b49
knesibu3@gmail.com <knesibu3@gmail.com>
2024-04-11 13:08:47 +0300

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,7 @@ if __name__ == "__main__":
     import hidden_4
 i = 0
 names = dir(hidden_4)
-while i <= len(names):
-    print("{}".format(names[i]))
+while i < len(names):
+    if names[i][:2] != '__':
+        print("{}".format(names[i]))
+    i += 1

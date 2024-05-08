ac09b5dc6dc72f310ef03860ff3f81ccf51b2fb1
knesibu3@gmail.com <knesibu3@gmail.com>
2024-05-08 22:10:10 +0300

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    length = 0
-    try:
-        for item in my_list:
-            print(item)
-            lengt
+def safe_print_list_list(my_list=[], x=0):
+    for i in my_list:
+        try:
+            print(i, end="")
+            x -= 1
+            if x == 0:
+                break
+        except Exception:
+            pass

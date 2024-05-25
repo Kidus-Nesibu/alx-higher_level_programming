c3a0c832056c767f490445789d492204c87581d1
knesibu3@gmail.com <knesibu3@gmail.com>
2024-05-25 15:52:02 +0300

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    result = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix)
-    return result
+if __name__ == "__main__":
+    def square_matrix_simple(matrix=[]):
+        result = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix)
+        return result

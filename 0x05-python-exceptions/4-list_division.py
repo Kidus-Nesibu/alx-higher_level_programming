55574795c09792b078ec4f1f74ee68ab7012ae2b
knesibu3@gmail.com <knesibu3@gmail.com>
2024-05-31 14:54:22 +0300

    task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             val = 0
         except TypeError:
             print("wrong type")
-            val iiii= 0
+            val = 0
         finally:
             new_list.append(val)
 

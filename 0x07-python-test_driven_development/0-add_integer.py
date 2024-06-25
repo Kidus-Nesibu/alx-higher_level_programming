fa54440f0b5041f45d2bc6d675a00ec7d88bc64e
Kidus-Nesibu <Knesibu3@gmail.com>
2024-06-26 00:29:24 +0300

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,22 @@
+#!/usr/bin/python3
+
+
+"""A module That a function to add integers"""
+
+
 def add_integer(a, b=98):
+    """Return the sum of integers
+    Args:
+        a: first argument
+        b: second argument
+
+    Return:
+        sum of the two arguments
+
+    Raise:
+        TyepError: if either of the arguments not an integer or a float
+
+    """
     if isinstance(a, float):
         a = int(a)
     elif isinstance(b, float):
@@ -7,4 +25,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int):
         raise TypeError("b must be an integer")
-    return (a + b)
+    return (int(a) + int(b))

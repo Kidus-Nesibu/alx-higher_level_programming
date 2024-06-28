caa66f269f08a54aed17e16bae85be3d0fe3a8c8
Kidus-Nesibu <Knesibu3@gmail.com>
2024-06-28 14:57:28 +0300

    task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""module containing a function that prints a given name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """ Concatinate and write the given argument as one
+    Args:
+        first_name: string argument containing first name
+        last_name: string argument containing last argument
+    Raises:
+        TypeError: first_name must be a string
+        TypeError: last_name must be a string
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
-        raise TypeErorr("last_name must be a string")
-    
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))

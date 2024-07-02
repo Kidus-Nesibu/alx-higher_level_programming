a66ddd9ee74cd4b1c04bf766d1ca933a8873c195
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-02 12:04:13 +0300

    task 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """This module defines a function that draws a square using a hash symbol"""
+
+
 def print_square(size):
     """A function that prints a square using # symbol
     Args:
-        size: number of the printed hash 
+        size: number of the printed hash
     Raises:
         TypeError: if size is not an integer
         ValueError: if size is less than zero
@@ -14,7 +16,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for i in range(size):
         for i in range(size):
             print("#", end="")

8fb1d00708931a31886be004788dbd0528764706
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-13 15:54:14 +0300

    .

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""define object attrivute lookup function"""
-
-
-def lookup(obj):
-    """return a list of available attribures"""
-    return (dir(obj))
--- 0x0A-python-inheritance/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines class BaseGeometry."""
-
-
-class BaseGeometry:
-    """Represent base geometry."""
-    def area(self):
-        "area which will be implemented"
-        raise Exception("area() is not implemented")

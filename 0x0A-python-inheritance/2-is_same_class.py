d4e63cc4f15a42213163d70409a6fade3243d1f4
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-13 17:37:18 +0300

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module that contain a function that determines instance of an object"""
+
+
+def is_same_class(obj, a_class):
+    """Function that Checks if obj is instance of a_class
+    Args:
+        obj: object created
+        a_class: a class to check against
+    """
+    return isinstance(obj, a_class)

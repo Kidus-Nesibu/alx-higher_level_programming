97f46aaee7c79e20c1030edbeb6c9045523d150a
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-19 16:24:43 +0300

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A Module that contain a funciton that decodes a Json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """A funciton that decodes a Json file"""
+    with open(filename, mode"r",) as file:
+        json.loads(filename)

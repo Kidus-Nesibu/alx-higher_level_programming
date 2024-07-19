45dbf5cc07ac5a34077e195700643b0ec3a4fe46
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-19 16:34:45 +0300

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def load_from_json_file(filename):
     """A funciton that decodes a Json file"""
     with open(filename, mode="r") as file:
-        return json.load(filename)
+        return json.load(file)

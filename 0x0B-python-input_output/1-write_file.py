e4d0f6fe5835dcb52412f63327b1acc46e6f3ef7
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-19 15:30:26 +0300

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=""):
     """Funciton that writes a text to a file"""
     with open(filename, mode="w", encoding="utf-8") as file:
-        file.write(text)
+       length = file.write(text)
+       return length

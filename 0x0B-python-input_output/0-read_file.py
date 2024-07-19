581d71770953e05b97557347a2a9546e9ba7fda0
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-19 13:49:32 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""A module containing a function that opens and read file"""
+
+
 def read_file(filename=""):
+    """Function that opens and read a file and display it"""
     with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read())

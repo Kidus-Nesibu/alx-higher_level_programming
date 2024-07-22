218ab9f135e9d937338f5d8237444caf06000db1
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-23 01:53:40 +0300

    task 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,48 +6,80 @@ from models.base import Base
 class Rectangle(Base):
     """A class that defines a Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        super().__init__(id)
+
+# getter functions
+#==============================================================================
 
     @property
     def width(self):
         """gets the value of the width"""
         return self.__width
-
-    @width.setter
-    def width(self, value):
-        """sets the value of the width"""
-        self.__width = value
-
+    
     @property
     def height(self):
         """gets the value of the height"""
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        """sets the value of the widht"""
-        self.__height = value
-
+    
     @property
     def x(self):
         """gets the value of x"""
         return self.__x
-
-    @x.setter
-    def x(self, value):
-        """sets the value of x"""
-        self.__x = value
-
+    
     @property
     def y(self):
         """gets the value of y"""
         return self.__y
 
+#setter funcitons
+#====================================================================================
+
+    @width.setter
+    def width(self, value):
+        """sets the value of the width"""
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        
+        self.__width = value
+
+
+    @height.setter
+    def height(self, value):
+        """sets the value of the height"""
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = value
+
+
+    @x.setter
+    def x(self, value):
+        """sets the value of x"""
+        if (type(value) is not int):
+            raise TypeError("x must be an integer")
+
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+        self.__x = value
+
     @y.setter
     def y(self, value):
         """sets value of y"""
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = value

41d1e9696e30dd50be34d845bba250ce457108da
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-23 02:40:26 +0300

    fixed the square module

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
         self.size = size
         self.x = x
         self.y = y
-        self.id = None
+        self.id = id
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -25,19 +25,19 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Sets the value for size"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         if value <= 0:
-            raise ValueError("width must be > 0")
+            raise ValueError("size must be > 0")
         self.__width = value
         self.__height = value
 
     def update(self, *args, **kwargs):
         """Updates attributes of an instance"""
 
-        if args is not None and len(args) != 0:
+        if args:
             if len(args) >= 1:
-                if type(args[0]) != int and args[0] is not None:
+                if args[0] is not None and not isinstance(args[0], int):
                     raise TypeError("id must be an integer")
                 self.id = args[0]
             if len(args) > 1:
@@ -49,20 +49,16 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 if key == "id":
-                    if type(value) != int and value is not None:
+                    if value is not None and not isinstance(value, int):
                         raise TypeError("id must be an integer")
                     self.id = value
-                if key == "size":
+                elif key == "size":
                     self.size = value
-                if key == "x":
+                elif key == "x":
                     self.x = value
-                if key == "y":
+                elif key == "y":
                     self.y = value
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Square"""
-
-        obj_dictionary = {'id': self.id, 'size': self.size, 'x': self.x,
-                          'y': self.y}
-
-        return obj_dictionary
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}

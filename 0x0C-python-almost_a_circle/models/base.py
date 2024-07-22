bc5b2f81cfbb19600c361d5d037aacee8e8b8c1e
Kidus-Nesibu <Knesibu3@gmail.com>
2024-07-23 02:57:09 +0300

    hot fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module contains a class to serve as base for other classes"""
 
-
 import csv
 import json
 import os
@@ -9,12 +8,12 @@ import turtle
 
 
 class Base:
-    """Represents base of all classes created """
+    """Represents base of all classes created"""
 
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ """
+        """Initialize the base class"""
         if id is not None:
             self.id = id
         else:
@@ -23,11 +22,11 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """returns the JSON representation of list_dictionaries"""
+        """Returns the JSON representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        if (type(list_dictionaries) != list or not
-                all(type(i) == dict for i in list_dictionaries)):
+        if (not isinstance(list_dictionaries, list) or
+                not all(isinstance(i, dict) for i in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return json.dumps(list_dictionaries)
 
@@ -40,24 +39,20 @@ class Base:
                 jsonfile.write("[]")
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
-                jsonfile.write(Base.to_json_string(list_dicts))
+                jsonfile.write(cls.to_json_string(list_dicts))
 
     @staticmethod
     def from_json_string(json_string):
         """Returns list of JSON string representations"""
-        json_string_list = []
-
-        if json_string is not None and json_string != '':
-            if type(json_string) != str:
-                raise TypeError("json_string must be a string")
-            json_string_list = json.loads(json_string)
-
-        return json_string_list
+        if json_string is None or json_string == '':
+            return []
+        if not isinstance(json_string, str):
+            raise TypeError("json_string must be a string")
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
         """Returns an instance with all attributes already set"""
-        # create an instance of an existing class
         if cls.__name__ == 'Rectangle':
             dummy = cls(1, 1)
         elif cls.__name__ == 'Square':
@@ -69,7 +64,6 @@ class Base:
     @classmethod
     def load_from_file(cls):
         """Returns a list of instances"""
-
         file_name = cls.__name__ + ".json"
         list_of_instances = []
         list_dictionaries = []
@@ -85,24 +79,6 @@ class Base:
     @classmethod
     def save_to_file_csv(cls, list_objs):
         """Serializes list_objs and saves to file"""
-
-        # if (type(list_objs) != list and list_objs is not None
-        #    or not all(isinstance(i, cls) for i in list_objs)):
-
-        #     raise TypeError("list_objs must be a list of instances")
-
-        # file_name = cls.__name__ + ".csv"
-        # with open(file_name, 'w') as my_file:
-        #     if list_objs is not None:
-        #         list_objs = [i.todictionary for i in list_objs]
-        #         if cls.__name__ == 'Rectangle':
-        #             records = ['id', 'width', 'height', 'x', 'y']
-        #         elif cls.__name__ == 'Square':
-        #             records = ['id', 'size', 'x', 'y']
-        #         script = csv.DictWriter(my_file, fieldnames=records)
-        #         script.writeheader()
-        #         script.writerows(list_objs)
-
         filename = cls.__name__ + ".csv"
         with open(filename, "w", newline="") as csvfile:
             if list_objs is None or list_objs == []:
@@ -113,31 +89,13 @@ class Base:
                 else:
                     fieldnames = ["id", "size", "x", "y"]
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
                 for obj in list_objs:
                     writer.writerow(obj.to_dictionary())
 
     @classmethod
     def load_from_file_csv(cls):
         """Deserializes CSV format from a file"""
-
-        # file_name = cls.__name__ + ".csv"
-        # list_of_instances = []
-        # if os.path.exists(file_name):
-        #     with open(file_name, 'r') as my_file:
-        #         reader = csv.reader(my_file, delimiter=',')
-        #         if cls.__name__ == 'Rectangle':
-        #             records = ['id', 'width', 'height', 'x', 'y']
-        #         elif cls.__name__ == 'Square':
-        #             records = ['id', 'size', 'x', 'y']
-        #         for i, row in enumerate(reader):
-        #             if i > 0:
-        #                 x = cls(1, 1)
-        #                 for j, y in enumerate(row):
-        #                     if y:
-        #                         setattr(x, records[j], int(y))
-        #                 list_of_instances.append(x)
-        # return list_of_instances
-
         filename = cls.__name__ + ".csv"
         try:
             with open(filename, "r", newline="") as csvfile:
@@ -146,8 +104,10 @@ class Base:
                 else:
                     fieldnames = ["id", "size", "x", "y"]
                 list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
-                list_dicts = [dict([k, int(v)] for k, v in d.items())
-                              for d in list_dicts]
+                list_dicts = [
+                    dict([k, int(v)] for k, v in d.items())
+                    for d in list_dicts
+                ]
                 return [cls.create(**d) for d in list_dicts]
         except IOError:
             return []
@@ -170,7 +130,7 @@ class Base:
             turt.up()
             turt.goto(rect.x, rect.y)
             turt.down()
-            for i in range(2):
+            for _ in range(2):
                 turt.forward(rect.width)
                 turt.left(90)
                 turt.forward(rect.height)
@@ -183,10 +143,8 @@ class Base:
             turt.up()
             turt.goto(sq.x, sq.y)
             turt.down()
-            for i in range(2):
-                turt.forward(sq.width)
-                turt.left(90)
-                turt.forward(sq.height)
+            for _ in range(4):  # Use 4 for squares
+                turt.forward(sq.size)
                 turt.left(90)
             turt.hideturtle()
 

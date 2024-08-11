c5e165851cf311449a5041b96884f40c5890768f
Kidus-Nesibu <Knesibu3@gmail.com>
2024-08-11 13:44:14 +0300

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- script that creates a database and user
-CREATE DATABASE IF NOT EXSISTS hbtn_od2;
+CREATE DATABASE IF NOT EXSISTS hbtn_0d2;
 CREATE USER IF NOT EXSISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
